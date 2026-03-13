55fc73ac98db8880799a9ec87939c9125d54ddcf
Rover <rover.stargazer1@gmail.com>
2026-03-13 15:38:44 +0800

    chore 注释

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-	readonly VITE_ENABLE_PATIENT_STREAM?: string
+	readonly VITE_ENABLE_PATIENT_STREAM?: string  //是否启用病人对话流式接口，支持以下任意一种方式启用：1、true、yes、on（不区分大小写）。默认为false，即不启用流式接口。
 }
 
 interface ImportMeta {
